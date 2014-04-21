761a16025b7fcb7768a764ecd7bb793d76d48ccc
Dan Rogers <dan@danro.net>
2014-04-21 15:59:19 -0700

    Fix transition from 'auto' in Safari

--- dist/tram.js
+++ dist/tram.js
@@ -1,5 +1,5 @@
 /*!
- * tram.js v0.7.6-global
+ * tram.js v0.7.7-global
  * Cross-browser CSS3 transitions in JavaScript
  * https://github.com/bkwld/tram
  * MIT License
@@ -754,8 +754,11 @@ window.tram = (function (jQuery) {
       this.active = true;
       value = this.convert(value, this.type);
       if (this.auto) {
-        // when transitioning from 'auto', we must always reset to computed
-        this.update(this.get());
+        // when transitioning from 'auto', we must reset to computed
+        if (this.el.style[this.name] == 'auto') {
+          this.update(this.get());
+          this.redraw();
+        }
         if (value == 'auto') value = getAuto.call(this);
       }
       this.nextStyle = value;
